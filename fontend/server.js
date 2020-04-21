3138b3862a8d7d0671a3d0b85fd0f1a99c147427
HienVDRK <hienvd@rikkeisoft.com>
2020-04-21 08:55:33 +0700

    commit 1

--- fontend/server.js
+++ fontend/server.js
@@ -13,4 +13,4 @@ res.sendFile(path.join(__dirname+'/dist/fontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 8080);
